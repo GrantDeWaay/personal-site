b7e664c78eccb3ceb4692f8ad023403bd7a8173e
grant <grantdewaay@gmail.com>
2025-02-06 11:54:55 -0600

    removed navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './Navbar';
 import CenteredVideo from './CenteredVideo';
 import GitHubIcon from './GitHubIcon';
 import LinkedInIcon from './LinkedInIcon';
